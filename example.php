dad2ebe989c7906f62c305eac3c9978126571a6c
Apperceive <markus.sandy@gmail.com>
2015-11-19 09:25:34 -0600

    add comments and sample output

--- example.php
+++ example.php
@@ -2,13 +2,13 @@
 
 $colb = array(
   'data' => 'b',
-  'header' => false,  // cell is a header
+  //'header' => FLASE,  // cell is a header, true if present, even if set to FALSE, ouputs th instead of td
   'class' => 'bclass bclass',
   'any-td-attr' => 'bvalue',
 );
 $cella2 = array(
   'data' => 'a2',
-  //'header' => false,  // cell is a header
+  //'header' => FLASE,  // cell is a header, true if present, even if set to FALSE, ouputs th instead of td
   'class' => 'aclass aaclass',
   'any-td-attr' => 'avalue',
 );
@@ -31,3 +31,21 @@ $vars = array(
 drupal_set_message(theme_table($vars));
 
 print theme_table($vars);
+
+/* output
+
+<table class="sticky-enabled">
+<caption>caption goes here</caption>
+ <thead><tr><th>a</th><th class="bclass bclass" any-td-attr="bvalue">b</th><th>c</th> </tr></thead>
+<tbody>
+ <tr class="odd"><td>a1</td><td>b1</td><td>c1</td> </tr>
+ <tr class="even"><td class="aclass aaclass" any-td-attr="avalue">a2</td><td>b2</td><td>c2</td> </tr>
+ <tr class="odd"><td>a3</td><td>b3</td><td>c3</td> </tr>
+ <tr class="even"><td>a4</td><td>b4</td><td>c4</td> </tr>
+</tbody>
+</table>
+
+*/
+
+
+
